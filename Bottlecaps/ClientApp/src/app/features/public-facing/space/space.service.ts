b4684f564f7cc2ff3c092f292782384e9fd05aa7
Stephen Printup <corky@corkysdevshop.com>
2020-05-31 16:01:39 -0600

    added space controller as per #5

---
+++ Bottlecaps/ClientApp/src/app/features/public-facing/space/space.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { DataService } from '../../../shared/data.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpaceService {
+
+	constructor(private dataService: DataService) { }
+}
