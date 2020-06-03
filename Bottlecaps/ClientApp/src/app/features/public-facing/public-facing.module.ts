7c299bfecf6a6ae0b85249c6a04e1bd9f4e6b13d
Stephen Printup <corky@corkysdevshop.com>
2020-06-03 17:52:44 -0600

    square bottlecap on top of background

--- Bottlecaps/ClientApp/src/app/features/public-facing/public-facing.module.ts
+++ Bottlecaps/ClientApp/src/app/features/public-facing/public-facing.module.ts
@@ -5,9 +5,10 @@ import { RouterModule } from '@angular/router';
 import { TagsComponent } from './space/tags/tags.component';
 import { LinksComponent } from './space/links/links.component';
 import { FormsModule } from '@angular/forms';
+import { SpaceAreaComponent } from './space/space-area/space-area.component';
 
 @NgModule({
-  declarations: [SpaceComponent, TagsComponent, LinksComponent],
+  declarations: [SpaceComponent, TagsComponent, LinksComponent, SpaceAreaComponent],
   imports: [
     CommonModule, RouterModule, FormsModule
 	],
---
+++ Bottlecaps/ClientApp/src/app/features/public-facing/space/space-area/space-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-space-area',
+  templateUrl: './space-area.component.html',
+  styleUrls: ['./space-area.component.css']
+})
+export class SpaceAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
