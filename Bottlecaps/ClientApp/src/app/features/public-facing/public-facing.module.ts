463bfd23cacafbf11d6a93e56b77d973f1ae5ab0
Stephen Printup <corky@corkysdevshop.com>
2020-06-05 08:16:05 -0600

    bottlecap can be dragged around screen

--- Bottlecaps/ClientApp/src/app/features/public-facing/public-facing.module.ts
+++ Bottlecaps/ClientApp/src/app/features/public-facing/public-facing.module.ts
@@ -6,11 +6,12 @@ import { TagsComponent } from './space/tags/tags.component';
 import { LinksComponent } from './space/links/links.component';
 import { FormsModule } from '@angular/forms';
 import { SpaceAreaComponent } from './space/space-area/space-area.component';
+import { MaterialModule } from '../../shared/material/material.module';
 
 @NgModule({
   declarations: [SpaceComponent, TagsComponent, LinksComponent, SpaceAreaComponent],
   imports: [
-    CommonModule, RouterModule, FormsModule
+    CommonModule, RouterModule, FormsModule, MaterialModule
 	],
 	exports: [
 		SpaceComponent
--- Bottlecaps/ClientApp/src/app/shared/material/material.module.ts
+++ Bottlecaps/ClientApp/src/app/shared/material/material.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatInputModule } from '@angular/material/input';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [],
 	exports: [
 		MatSnackBarModule,
-    
+    DragDropModule
 	]
 })
 export class MaterialModule { }
