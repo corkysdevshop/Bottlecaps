c110a76fc8430f79132a466d84fa94df7695915f
Stephen Printup <corky@corkysdevshop.com>
2020-07-31 18:09:15 -0600

    code cleanup

--- Bottlecaps/ClientApp/src/app/features/public-facing/space/space-area/space-area.component.ts
+++ Bottlecaps/ClientApp/src/app/features/public-facing/space/space-area/space-area.component.ts
@@ -16,24 +16,16 @@ export class SpaceAreaComponent implements OnInit {
   constructor(private spaceService: SpaceService) { }
 
 	ngOnInit(): void {
-		
-
 		this.spaceService.spacesChanged.subscribe(spaces => {
 			this.spaceCaps = [];
 			console.log("111111111111111", spaces);
 			this.loadPositions(spaces);
 			this.spaceCaps = spaces.slice();
-		}, (err) => { console.log("err: ", err); },
-			() => {
-            console.log("success!!!")
-				for (var i = 0; i < this.spaceCaps.length; i++) {
-					console.log("space:", this.spaceCaps[i]);
-				}
-			}
-		)
+		}, (err) => { console.log("err: ", err); });
 		this.spaceService.getSpaceBottlecaps();
   }
 
+  // puts position objects into local array so they can be accessed in html cdkDragFreeDragPosition to set the position on load. 
 	loadPositions(spaces: Bottlecap[]) {
 		this.dragPosition = [];
 		for (var i = 0; i < spaces.length; i++) {
@@ -43,18 +35,19 @@ export class SpaceAreaComponent implements OnInit {
 		}
 	}
 
+  // figures out where bottlecap was moved to
 	onDragEnded(event, bottlecap: Bottlecap) {
 		console.log(event, bottlecap);
 		var oldX = parseInt(bottlecap.positionX);
 		var oldY = parseInt(bottlecap.positionY);
 		var newX = event.distance.x;
 		var newY = event.distance.y;
-	/**/
 		var convertedX = oldX + newX;
 		var convertedY = oldY + newY;
 		this.updatePlace(Math.round(convertedX), Math.round(convertedY), bottlecap.bottlecapId);
 	}
 
+  // sends new position to database
 	updatePlace(x: number, y: number, spaceCapId: number) {
 		const space = new Space();
 		space.SpaceId = spaceCapId.toString();
@@ -62,9 +55,11 @@ export class SpaceAreaComponent implements OnInit {
 		space.PositionY = y.toString();
 		console.log("updatePlace x: ", x, " y: ", y);
 		this.spaceService.updatePlace(space);
-		this.spaceService.getSpaceBottlecaps();
+		//this.spaceService.getSpaceBottlecaps();
 	}
 
+  // END
+  // HELPER METHODS FOR DEVELOPMENT
 	checkSpaces(event) {
 		//console.log("spaces in this.spaceService.spaceBottleCapCollection: ", this.spaceService.spaceBottleCapCollection);
 		console.log("spaces in this.spaceCaps: ", this.spaceCaps);
@@ -76,14 +71,5 @@ export class SpaceAreaComponent implements OnInit {
 		for (let space of this.spaceCaps) {
 			this.updatePlace(10, 10, space.bottlecapId);
 		}
-	}
-	setDragPosition(space) {
-		//var newPositionString = "{x:" + space.positionX + ",y:" + space.positionY + "}";
-        // TODO: THIS STRING NEEDS TO GO INTO THE [cdkDragFreeDragPosition], but it won't allow template binding on the one hand and I can't instantiate multiple local variables from the for loop on the other
-		//return newPositionString;
-		//var posObj = { x: space.positionX, y: space.positionY };
-		////console.log("**setDragPosition:", newPositionString, "space.bottlecapId: ", space.bottlecapId);
-		//console.log("**setDragPosition:", posObj, "space.bottlecapId: ", space.bottlecapId); //return {x:0,y:0}
-		//this.dragPosition.push(posObj);
 	}
 }
